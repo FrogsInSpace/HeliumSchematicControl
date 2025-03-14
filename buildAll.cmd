bf3d2783884ef924736852582d440585a26d11d9
Josef Wienerroither <spacefrog@frogsinspace.at>
2025-03-14 07:44:14 +0100

    improve project setup add 3ds Max 2027 build target

--- buildAll.cmd
+++ buildAll.cmd
@@ -1,5 +1,5 @@
 @echo off
-FOR /L %%X IN (2017,1,2026) DO (
+FOR /L %%X IN (2017,1,2027) DO (
 	if NOT DEFINED ADSK_3DSMAX_SDK_%%X CONTINUE
 	MSBuild PS_Schematic.sln /P:Configuration=Release-Max%%X
 	MSBuild PS_Schematic.sln /P:Configuration=Release-Max%%X /P:KRAKATOA_BUILD=Krakatoa
